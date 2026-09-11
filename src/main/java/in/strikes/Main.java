20b73082f10fe62ef2bce1e528d68f5812858110
Suhail <suhail@methuselahventures.ai>
2026-09-12 00:05:11 +0530

    boiler plate code for running the application with minor bug correction in controller class

--- src/main/java/in/strikes/Main.java
+++ src/main/java/in/strikes/Main.java
@@ -1,14 +1,18 @@
 package in.strikes;
 
+import in.strikes.config.WebConfig;
 import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.File;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LifecycleException {
 
         // boilerplate code
 
@@ -19,10 +23,26 @@ public class Main {
         tomcat.getConnector();
 
         String contextPath = "";
-        String baseDoc = new File(
-                System.getProperty("src/main/webapp")
-        ).getAbsolutePath();
+        String baseDoc = new File("src/main/webapp").getAbsolutePath();
 
         Context context = tomcat.addContext(contextPath , baseDoc);
+
+        // IOC container app
+        AnnotationConfigWebApplicationContext springContext = new AnnotationConfigWebApplicationContext();
+        springContext.register(WebConfig.class);
+
+        // dispatcher servlet
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(springContext);
+
+        Tomcat.addServlet(context , "dispatcherServlet" , dispatcherServlet);
+
+        context.addServletMappingDecoded("/" , "dispatcherServlet");
+
+        tomcat.start();
+
+        System.out.println("Tomcat running on port 8080");
+
+        //keep server running
+        tomcat.getServer().await();
     }
 }
--- src/main/java/in/strikes/controller/StudentController.java
+++ src/main/java/in/strikes/controller/StudentController.java
@@ -4,10 +4,7 @@ import in.strikes.entity.Student;
 import in.strikes.service.StudentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,8 +27,8 @@ public class StudentController {
         return ResponseEntity.ok(studentResp);
     }
 
-    @GetMapping
-    public ResponseEntity<Student> getStudentById(Long id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") Long id) {
         Student studentResp = studentService.getStudent(id);
 
         if (studentResp == null) {
